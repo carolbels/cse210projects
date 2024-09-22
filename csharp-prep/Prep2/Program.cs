32f401c7f835f2ee5f11042b554173e15662c397
carolbels <145243580+carolbels@users.noreply.github.com>
2024-09-21 20:15:30 -0500

    Completed the activity

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percente = int.Parse(answer);
+
+        string letter = "";
+
+        if (percente >=90)
+        {
+            letter = "A";
+        }
+        else if (percente  >= 80)
+        {
+            letter = "B";
+        }
+        else if (percente  >= 70)
+        {
+            letter = "C";
+        }
+        else if (percente  >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+        Console.WriteLine ($"Your grade is : {letter}");
+
+        if (percente >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!")
+        }
     }
 }
